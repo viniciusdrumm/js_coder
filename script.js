0a76d5e2ad71018e8bc834d6b57e6ee7af6eae25
Vinicius Drumm <dev.viniciusdrumm@gmail.com>
2024-05-13 19:32:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var saudacao = 'Seja bem-vindo, '
+var nome = prompt('Digite o seu nome');
+console.log(saudacao + nome + "!");
+
+var anoAtual = 2024;
+var anoNascimento = prompt('Insira seu ano de nascimento');
+
+var idade = anoAtual - anoNascimento;
+
+console.log('Você tem ' + idade + ' anos.')
+
