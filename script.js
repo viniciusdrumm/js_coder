f64fee47ec7f0f77b9148cd9455fd00b6888c348
Vinicius Drumm <dev.viniciusdrumm@gmail.com>
2024-05-13 19:34:42 -0300

    Exercício complementar - Aula 02

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var nome = prompt('Digite o seu nome');
 console.log(saudacao + nome + "!");
 
 var anoAtual = 2024;
-var anoNascimento = prompt('Insira seu ano de nascimento');
+var anoNascimento = parseInt(prompt('Insira seu ano de nascimento'));
 
 var idade = anoAtual - anoNascimento;
 
